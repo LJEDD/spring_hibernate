e3a98896275c51b15354fcf81bb1e11b3092c868
Karim <karim.summary@gmail.com>
2022-10-06 00:42:46 +0300

    with_method

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,12 +1,15 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 import java.util.List;
+
+
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
@@ -15,10 +18,24 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+
+      User user1 = (new User("Name1", "LastN1", "int1@m.com"));
+      User user2 = (new User("Name2", "LastN2", "int2@m.com"));
+      User user3 = (new User("Name3", "LastN3", "int3@m.com"));
+      User user4 = (new User("Name4", "LastN4", "int4@m.com"));
+
+
+      Car car1 = new Car("car1", 1900);
+      Car car2 = new Car("car2", 1901);
+      Car car3 = new Car("car3", 1902);
+      Car car4 = new Car("car4", 1903);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +45,8 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+
+      System.out.println("Koordinati voditelya :"+" "+userService.getUserByCar("car1", 1900));
 
       context.close();
    }
